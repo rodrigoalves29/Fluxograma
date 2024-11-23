23051742c51d3bfc2ed69f360970ebaaa7ceea16
rodrigoalves29 <rodrigoalves212134@gmail.com>
2024-11-23 16:26:14 -0300

    Add files via upload

---
+++ PYTHON/aula_5.py
@@ -0,0 +1,73 @@
+# por padrão o input é um texto
+# numero1 = input ('Digite um número:')
+# numero2 = input ('Digite um número:')
+# soma = numero1 + numero2
+# tipo = type (numero1)
+# print(soma)
+
+nome = input ('Digite seu nome:')
+print('SEJA BEM VINDO(A)', nome)
+print('SEJA BEM VINDO(A)'+ nome)
+print('SEJA BEM VINDO(A) {nome}')
+print('SEJA BEM VINDO(A){}'. format(nome))
+print('SEJA BEM VINDO(A) %s'% (nome))
+
+#AULA 5 Atividade 1
+print('SEJA BEM VINDO(A)')
+
+# Atividade 2
+#booleana = True
+#print(booleana)
+
+
+# Atividade 3
+numero1 = float (input('valor do numero'))
+numero2 = float (input('valor do numero'))
+mult = numero1 * numero2
+print(mult)
+
+# Atividade 4
+numero1 = float (input('valor do numero'))
+numero2 = float (input('valor do numero'))
+div = numero1 / numero2
+print(div)
+
+# Atividade 5
+numero1 = float (input('valor do numero'))
+numero2 = float (input('valor do numero'))
+sub = numero1 - numero2
+print(sub)
+
+# Atividade 6
+numero1 = float (input('valor do numero'))
+numero2 = float (input('valor do numero'))
+div = numero1 / numero2
+print(div)
+
+# Atividade 7
+numero1 = float (input('valor do numero'))
+numero2 = float (input('valor do numero'))
+numero3 = float (input('valor do numero'))
+numero4 = float (input('valor do numero'))
+mult = numero1 * numero2 * numero3 * numero4
+print(mult)
+
+# Atividade 8
+numero_int = 16
+#print(numero_int**4)
+
+# Atividade 9
+numero1 = float (input('valor do numero'))
+numero2 = float (input('valor do numero'))
+soma = numero1 + numero2
+sub = numero1 - numero2
+mult = numero1 * numero2
+div = numero1 / numero2
+print(soma, sub, mult, div)
+
+# Atividade 10
+nome = input('Digite seu nome')
+idade = input('Digite sua idade')
+email = input('Digite seu email')
+endereco = input('Digite seu endereco')
+print(nome, idade, email, endereco)
